0a09d53f9658782efd86a9d5927dd94d2cb08711
guidoarnone <guido.arnone@mercadolibre.com>
2015-04-18 18:32:51 -0300

    Added Wave and Spawner classes.

---
+++ game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] waves;
+	public float waveInterval;
+	private Queue<GameObject> waveQueue;
+	
+	// Use this for initialization
+	public void Start () {
+		waveQueue = new Queue<GameObject> ();
+		startWaves();
+	}
+	
+	private void startWaves()
+	{
+		foreach(GameObject w in waves)
+		{
+			waveQueue.Enqueue(w);
+		}
+		StartCoroutine(startWave());
+	}
+	
+	private IEnumerator startWave() {
+
+		waveQueue.Dequeue().GetComponent<Wave>().execute();
+
+		yield return new WaitForSeconds(waveInterval);
+		
+		if (waveQueue.Count > 0) 
+		{
+			StartCoroutine(startWave());
+		}
+		
+	}
+}
