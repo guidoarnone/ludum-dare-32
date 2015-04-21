dd22aa274a937e3ac668226a51d4e2a0899ba131
guidoarnone <guido.arnone@mercadolibre.com>
2015-04-20 23:18:20 -0300

    mac build

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$('#web-build').attr('href', './webgame/game.html')
 	$('#windows-build').attr('href', 'https://mega.co.nz/#!ylgTlLhb!ruzd9KAqb1Gdo579O6Vwl-pdN90H9o8GTWOvloHoGqE')
-	$('#osx-build').attr('href', '')
+	$('#osx-build').attr('href', 'https://mega.co.nz/#!r4ZQjRQJ!CagfPa7yMWPo_9DgM0tnPzPT0qwROuFGLv2OX884ZJY ')
 	$('#linux-build').attr('href', 'https://mega.co.nz/#!CtoBSIoZ!8K8ATw9u7XvfibwQTFns6icSSw3CkuWXma1xena6s6s')
 	$('#github-repo').attr('href','https://github.com/guidoarnone/ludum-dare-32')
 })
