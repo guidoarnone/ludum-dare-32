3c3a607e72004b35a4c3ccc11437ec7f02d7a6da
guidoarnone <guido.arnone@mercadolibre.com>
2015-04-20 23:00:46 -0300

    added windows build

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('#web-build').attr('href', './webgame/game.html')
-	$('#windows-build').attr('href', '')
+	$('#windows-build').attr('href', 'https://mega.co.nz/#!ylgTlLhb!ruzd9KAqb1Gdo579O6Vwl-pdN90H9o8GTWOvloHoGqE')
 	$('#osx-build').attr('href', '')
 	$('#linux-build').attr('href', 'https://mega.co.nz/#!CtoBSIoZ!8K8ATw9u7XvfibwQTFns6icSSw3CkuWXma1xena6s6s')
 	$('#github-repo').attr('href','https://github.com/guidoarnone/ludum-dare-32')
