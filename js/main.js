158c16d91da6003caa26e28a4cf867a44f8c406d
guidoarnone <guido.arnone@mercadolibre.com>
2015-04-20 22:52:40 -0300

    added link to linux build

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
 	$('#web-build').attr('href', './webgame/game.html')
+	$('#windows-build').attr('href', '')
+	$('#osx-build').attr('href', '')
+	$('#linux-build').attr('href', 'https://mega.co.nz/#!CtoBSIoZ!8K8ATw9u7XvfibwQTFns6icSSw3CkuWXma1xena6s6s')
 	$('#github-repo').attr('href','https://github.com/guidoarnone/ludum-dare-32')
 })
